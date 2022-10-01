eb80866f08d75e07f6ac27102e914f517baefa7c
slopiano <sebastian_lopiano@hotmail.com>
2022-10-01 16:31:32 -0400

    test

--- GitHubApi-HW4a/test_github.py
+++ GitHubApi-HW4a/test_github.py
@@ -5,7 +5,7 @@ from github import get_user
 def test_richkempinski(capsys):
     get_user('richkempinski')
     captured = capsys.readouterr()
-    assert captured.out == 'Repo: csp Number of commits: 2\n\
+    assert captured.out == 'Repo: csp Number of commits : 2\n\
 Repo: hellogitworld Number of commits: 30\n\
 Repo: helloworld Number of commits: 6\n\
 Repo: Mocks Number of commits: 10\n\
@@ -13,4 +13,4 @@ Repo: Project1 Number of commits: 2\n\
 Repo: richkempinski.github.io Number of commits: 9\n\
 Repo: threads-of-life Number of commits: 1\n\
 Repo: try_nbdev Number of commits: 2\n\
-Repo: try_nbdev2 Number of commits: 5'
+Repo: try_nbdev2 Number of commits: 5\n'
