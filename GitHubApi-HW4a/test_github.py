15a49fe03be8e25462da981378c92085c11d370d
slopiano <slopiano@stevens.edu>
2022-10-06 09:42:55 -0400

    confused

--- GitHubApi-HW4a/test_github.py
+++ GitHubApi-HW4a/test_github.py
@@ -1,16 +1,20 @@
 import unittest
+from unittest.mock import Mock, patch
 import pytest
 from github import get_user
 
-def test_richkempinski(capsys):
-    get_user('richkempinski')
-    captured = capsys.readouterr()
-    assert captured.out == 'Repo: csp Number of commits: 2\n\
-Repo: hellogitworld Number of commits: 30\n\
-Repo: helloworld Number of commits: 6\n\
-Repo: Mocks Number of commits: 10\n\
-Repo: Project1 Number of commits: 2\n\
-Repo: richkempinski.github.io Number of commits: 9\n\
-Repo: threads-of-life Number of commits: 1\n\
-Repo: try_nbdev Number of commits: 2\n\
-Repo: try_nbdev2 Number of commits: 5\n'
+def mock_testing_rich(mock_get):
+    with patch('github.requests.get') as mock_get:
+
+# def test_richkempinski(capsys):
+#     get_user('richkempinski')
+#     captured = capsys.readouterr()
+#     assert captured.out == 'Repo: csp Number of commits: 2\n\
+# Repo: hellogitworld Number of commits: 30\n\
+# Repo: helloworld Number of commits: 6\n\
+# Repo: Mocks Number of commits: 10\n\
+# Repo: Project1 Number of commits: 2\n\
+# Repo: richkempinski.github.io Number of commits: 9\n\
+# Repo: threads-of-life Number of commits: 1\n\
+# Repo: try_nbdev Number of commits: 2\n\
+# Repo: try_nbdev2 Number of commits: 5\n'
